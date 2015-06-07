3e6f48809a2cb818a7430797a74c127b6ecd6e6f
terjetyl <terjetyl@gmail.com>
2015-06-07 23:03:53 +0200

    init

---
+++ Mod10.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace ModulusCalculator.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Mod10Test()
+        {
+            Assert.Equal(6, Mod10.GetControlNumber("234567"));
+            Assert.Equal(9, Mod10.GetControlNumber("020130096"));
+            Assert.Equal("2345676", Mod10.Calculate("234567"));
+            Assert.Equal("0201300969", Mod10.Calculate("020130096"));
+        }
+
+        [Fact]
+        public void Mod11Test()
+        {
+            Assert.Equal(6, Mod11.GetControlNumber("234567"));
+            Assert.Equal(7, Mod11.GetControlNumber("020130096"));
+            Assert.Equal("2345676", Mod11.Calculate("234567"));
+            Assert.Equal("0201300967", Mod11.Calculate("020130096"));
+        }
+    }
+}
---
+++ Mod10/PortableLibrary1.fs
@@ -0,0 +1,91 @@
+﻿namespace ModulusCalculator
+
+module private Helper = 
+    
+    open System
+    open System.Globalization
+
+    let charToInt (x:char) = 
+        Int32.Parse(x.ToString())
+
+    let convertToCharArray (str:String) = 
+        str.ToCharArray()
+
+    let revertArray arr = 
+        Array.rev arr
+
+    let convertToNumArray (charArray:char[]) = 
+        charArray |> Array.map charToInt
+
+    let doubleEverySecondNumber arr =
+        arr |> Seq.mapi (fun i x -> 
+                            match i%2 with
+                            | 0 -> x*2
+                            | _ -> x)
+
+    let splitNumber x = 
+        match x with 
+        | a when a > 9 -> 1 + (a-10)
+        | _ -> x
+
+    let splitNumbersOver10 arr = 
+        arr |> Seq.map splitNumber
+    
+module Mod10 =
+    
+    open System
+    open Helper
+
+    let GetControlNumber modbase = 
+        if(String.IsNullOrWhiteSpace(modbase)) then
+            failwith "modbase was null or empty"
+
+        let modnum = modbase
+                        |> convertToCharArray
+                        |> revertArray 
+                        |> convertToNumArray
+                        |> doubleEverySecondNumber
+                        |> splitNumbersOver10
+                        |> Seq.sum
+
+        let controlnumber = 10 - (modnum%10)
+        controlnumber
+
+    let Calculate modbase = 
+        let controlnumber = GetControlNumber modbase
+
+        match controlnumber with
+        | 10 -> modbase + "0"
+        | _ -> modbase + controlnumber.ToString()
+
+module Mod11 =
+
+    open System
+    open Helper
+
+    let GetControlNumber modbase = 
+        if(String.IsNullOrWhiteSpace(modbase)) then
+            failwith "modbase was null or empty"
+
+        let weightNumbers = [ 2; 3; 4; 5; 6; 7 ]
+
+        let modnum num weightNumber = 
+            num*weightNumber
+
+        let sum = modbase
+                    |> convertToCharArray
+                    |> revertArray 
+                    |> convertToNumArray
+                    |> Seq.mapi (fun i x -> modnum x weightNumbers.[i%6])
+                    |> Seq.sum
+
+        let controlnumber = 11 - (sum % 11)
+        controlnumber
+
+    let Calculate modbase = 
+    
+        let controlnumber = GetControlNumber modbase
+
+        match controlnumber with
+        | 11 -> modbase + "0"
+        | _ -> modbase + controlnumber.ToString()
